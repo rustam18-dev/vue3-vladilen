ed9e60015932c8f4fd2aa0fcffdc02dca1ac8ad6
Radzkhabov Rustam <rustam.radzabov8410@gmail.com>
2023-07-22 17:09:03 +0500

    qweqwe

---
+++ 9) Vue Router/vue-router/src/views/Forget.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <h2>Забыл пароль? Так вспоминай!</h2>
+
+    <button class="btn" >Обратно к логину</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
