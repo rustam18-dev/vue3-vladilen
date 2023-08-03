f66bed62cd1c6c037a6d4d3b3365f1539d38e491
Radzkhabov Rustam <rustam.radzabov8410@gmail.com>
2023-08-03 20:03:21 +0500

    rr
    rr

--- 11) Composition API/vue-composition/src/App.vue
+++ 11) Composition API/vue-composition/src/App.vue
@@ -1,29 +1,88 @@
 <template>
   <div class="container">
     <div class="card">
-      <h1>Vue Composition Api</h1>
+      <h1>Vue Composition Api </h1>
+      <small>data, methods, computed, watch</small>
       <hr>
       <p>Название: <strong>{{ name }}</strong></p>
-      <p>Версия: <strong>{{ version }}</strong></p>
+      <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong></p>
 
-      <button class="btn" @click="changeInfo">Изменить</button>
+      <button class="btn" @click="change">Изменить</button>
     </div>
   </div>
 </template>
 
 <script>
+import {
+    ref,
+    reactive,
+    toRefs,
+    isRef,
+    isReactive,
+    computed,
+    watch,
+
+} from 'vue'
+
 export default {
-  data() {
-    return {
+  setup() {
+    const name = ref('VueJs')
+    const version = ref(3)
+
+    // console.log(isRef(name))
+    // console.log(isRef(version.value))
+
+    const framework = reactive({
       name: 'VueJS',
-      version: 3
+      version: 3,
+    })
+
+    // const doubleVersion = computed(() => {
+    //   return version.value * 2
+    // })
+
+    const doubleVersion = computed(() => version.value * 2)
+
+    // console.log(isRef(doubleVersion))
+    // console.log(doubleVersion.value)
+
+
+    // console.log(isReactive(framework))
+    // console.log(isReactive(framework.name))
+
+    watch([doubleVersion, name], (newValues, oldValues) => {
+    console.log('new doubleVersion', newValues[0]);
+    console.log('new name', newValues[1]);
+    console.log('old doubleVersion', oldValues[0]);
+    console.log('old name', oldValues[1]);
+    })
+
+    function changeInfo() {
+      name.value = 'Vue JS!';
+      version.value = 42;
+      // framework.name = 'Vue JS!';
+      // framework.version = 42;
     }
+
+    return {
+      name: name,
+      version: version,
+      change: changeInfo,
+      doubleVersion,
+    }
+
   },
-  methods: {
-    changeInfo() {
-      this.name = 'Vue JS!'
-      this.version = 4
-    }
-  }
+  // data() {
+  //   return {
+  //     name: 'VueJS',
+  //     version: 3
+  //   }
+  // },
+  // methods: {
+  //   changeInfo() {
+  //     this.name = 'Vue JS!'
+  //     this.version = 4
+  //   }
+  // }
 }
 </script>
