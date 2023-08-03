4f88dcd8b05bf8fd10691d441324905dab90b31a
Radzkhabov Rustam <rustam.radzabov8410@gmail.com>
2023-08-04 00:03:49 +0500

    21

--- 11) Composition API/vue-composition/src/App.vue
+++ 11) Composition API/vue-composition/src/App.vue
@@ -3,16 +3,22 @@
     <div class="card">
       <h1>Vue Composition Api </h1>
       <small>data, methods, computed, watch</small>
-      <hr>
-      <p>Название: <strong>{{ name }}</strong></p>
-      <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong></p>
-
+      <div class="form-control">
+        <input type="text" v-model="firstName">
+      </div>
       <button class="btn" @click="change">Изменить</button>
     </div>
+
+    <FrameworkInfo
+        :name="name"
+        :version="version"
+        @change-version="changeVersion"
+    />
   </div>
 </template>
 
 <script>
+import FrameworkInfo from "@/FrameworkInfo";
 import {
     ref,
     reactive,
@@ -28,61 +34,43 @@ export default {
   setup() {
     const name = ref('VueJs')
     const version = ref(3)
+    const firstName = ref('');
 
-    // console.log(isRef(name))
-    // console.log(isRef(version.value))
+    // const doubleVersion = computed(() => version.value * 2)
 
-    const framework = reactive({
-      name: 'VueJS',
-      version: 3,
-    })
-
-    // const doubleVersion = computed(() => {
-    //   return version.value * 2
+    // watch([doubleVersion, name], (newValues, oldValues) => {
+    // console.log('new doubleVersion', newValues[0]);
+    // console.log('new name', newValues[1]);
+    // console.log('old doubleVersion', oldValues[0]);
+    // console.log('old name', oldValues[1]);
     // })
 
-    const doubleVersion = computed(() => version.value * 2)
-
-    // console.log(isRef(doubleVersion))
-    // console.log(doubleVersion.value)
-
-
-    // console.log(isReactive(framework))
-    // console.log(isReactive(framework.name))
-
-    watch([doubleVersion, name], (newValues, oldValues) => {
-    console.log('new doubleVersion', newValues[0]);
-    console.log('new name', newValues[1]);
-    console.log('old doubleVersion', oldValues[0]);
-    console.log('old name', oldValues[1]);
+    watch(firstName, (newV, oldV) => {
+      console.log(newV);
+      console.log(oldV);
     })
 
     function changeInfo() {
       name.value = 'Vue JS!';
       version.value = 42;
-      // framework.name = 'Vue JS!';
-      // framework.version = 42;
+
+    }
+
+    function changeVersion(number) {
+      version.value = number
     }
 
     return {
-      name: name,
-      version: version,
+      name,
+      version,
       change: changeInfo,
-      doubleVersion,
+      firstName,
+      changeVersion,
     }
 
   },
-  // data() {
-  //   return {
-  //     name: 'VueJS',
-  //     version: 3
-  //   }
-  // },
-  // methods: {
-  //   changeInfo() {
-  //     this.name = 'Vue JS!'
-  //     this.version = 4
-  //   }
-  // }
+  components: {
+    FrameworkInfo
+  }
 }
 </script>
---
+++ 11) Composition API/vue-composition/src/FrameworkInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <hr>
+    <p>Название: <strong>{{ name }}</strong></p>
+    <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong></p>
+
+
+    <button class="btn warning" @click="changeToThree">Изменить на 3 версию</button>
+  </div>
+</template>
+
+<script>
+import {computed} from "vue";
+
+export default {
+  props: ['name', 'version'],
+  emits: ['change-version'],
+  setup(props, context) {
+
+    function change() {
+      context.emit('change-version', 3)
+    }
+
+    return {
+      changeToThree: change,
+      doubleVersion: computed( () => props.version * 2)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
